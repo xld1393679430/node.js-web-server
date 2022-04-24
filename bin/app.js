3650b4e8949de80c244ad9182c98e73d1f7ece40
ld_xu <ld_xu@trip.com>
2022-04-24 12:20:55 +0800

    创建blog和user路由

--- bin/app.js
+++ bin/app.js
@@ -1,14 +1,30 @@
+const handleUserRouter = require("../src/router/user");
+const handleBolgRouter = require("../src/router/blog");
+
 const serverHandle = (req, res) => {
-    // 设置返回格式 JSON
-    res.setHeader('Content-type', 'application/json')
+  // 设置返回格式 JSON
+  res.setHeader("Content-type", "application/json");
 
-    const data = {
-        name: 'ssss',
-        age: 102,
-        env: process.env.NODE_ENV
-    }
+  // 处理博客路由
+  const blogData = handleBolgRouter(req, res);
+  console.log(blogData, '----blogData')
+  if (blogData) {
+    res.end(JSON.stringify(blogData));
+    return;
+  }
 
-    res.end(JSON.stringify(data))
+  // 处理用户路由
+  const userData = handleUserRouter(req, res);
+  console.log(userData, '----userData')
+  if (userData) {
+    res.end(JSON.stringify(userData));
+    return;
+  }
+
+  // 未命中 404
+  res.writeHead(404, { "Content-type": "text/plain" });
+  res.write("404 not found");
+  res.end();
 };
 
 module.exports = serverHandle;
---
+++ src/router/blog.js
@@ -0,0 +1,42 @@
+const handleBolgRouter = (req, res) => {
+  const method = req.method;
+  const url = req.url;
+  const path = url.split("?")[0];
+
+  // 获取博客列表
+  if (method === "GET" && path === "/api/blog/list") {
+    return {
+      msg: "博客列表",
+    };
+  }
+
+  // 获取博客详情
+  if (method === "GET" && path === "/api/blog/detail") {
+    return {
+      msg: "博客详情",
+    };
+  }
+
+  // 新建一篇博客
+  if (method === "POST" && path === "/api/blog/new") {
+    return {
+      msg: "新建一篇博客",
+    };
+  }
+
+  // 更新一篇博客
+  if (method === "POST" && path === "/api/blog/update") {
+    return {
+      msg: "更新博客",
+    };
+  }
+
+  // 删除一篇博客
+  if (method === "POST" && path === "/api/blog/delete") {
+    return {
+      msg: "删除博客",
+    };
+  }
+};
+
+module.exports = handleBolgRouter;
