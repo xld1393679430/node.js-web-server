f535689deed3f5795d072354c6650ac4f037217e
ld_xu <ld_xu@trip.com>
2022-04-27 12:41:11 +0800

    前端后端联调 & 配置Nginx

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,10 @@
 const http = require("http");
-const serverHandle = require('./app')
-const PORT = 3000;
+const serverHandle = require("./app");
+const PORT = 3001;
 const server = http.createServer(serverHandle);
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`now is listen ${PORT}`);
+});
 
 module.exports = server;
